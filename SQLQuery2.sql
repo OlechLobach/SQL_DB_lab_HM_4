2c5390ed0930da2e6576ec1e60c45de0dc11aead
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-05-20 03:32:42 +0300

    Initiall commit

---
+++ SQLQuery2.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE Hospital;
+GO
+
+USE Hospital;
+GO
+
+CREATE TABLE Departments (
+    Id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    Building INT NOT NULL CHECK (Building BETWEEN 1 AND 5),
+    Financing MONEY NOT NULL DEFAULT 0 CHECK (Financing >= 0),
+    Floor INT NOT NULL CHECK (Floor >= 1),
+    Name NVARCHAR(100) NOT NULL UNIQUE CHECK (Name != '')
+);
+GO
+
+CREATE TABLE Diseases (
+    Id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    Name NVARCHAR(100) NOT NULL UNIQUE CHECK (Name != ''),
+    Severity INT NOT NULL DEFAULT 1 CHECK (Severity >= 1)
+);
+GO
+
+CREATE TABLE Doctors (
+    Id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    Name NVARCHAR(MAX) NOT NULL CHECK (Name != ''),
+    Phone CHAR(10) NOT NULL,
+    Premium MONEY NOT NULL DEFAULT 0 CHECK (Premium >= 0),
+    Salary MONEY NOT NULL CHECK (Salary > 0),
+    Surname NVARCHAR(MAX) NOT NULL CHECK (Surname != '')
+);
+GO
+
+CREATE TABLE Examinations (
+    Id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    DayOfWeek INT NOT NULL CHECK (DayOfWeek BETWEEN 1 AND 7),
+    EndTime TIME NOT NULL CHECK (EndTime > StartTime),
+    Name NVARCHAR(100) NOT NULL UNIQUE CHECK (Name != ''),
+    StartTime TIME NOT NULL CHECK (StartTime BETWEEN '08:00' AND '18:00')
+);
+GO
+
+CREATE TABLE Wards (
+    Id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    Building INT NOT NULL CHECK (Building BETWEEN 1 AND 5),
+    Floor INT NOT NULL CHECK (Floor >= 1),
+    Name NVARCHAR(20) NOT NULL UNIQUE CHECK (Name != '')
+);
